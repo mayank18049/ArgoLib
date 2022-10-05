711c9aa59d66a5a4f2bf9acdf4e05054ea609328
Mayank Rawat <mayank18049@iiitd.ac.in>
2022-10-05 17:14:02 +0530

    Add Skeleton Code

---
+++ include/argolib.hpp
@@ -0,0 +1,31 @@
+#ifndef __ARGOLIB_H__
+#define __ARGOLIB_H__
+#include "abt.h"
+namespace argolib{
+typedef ABT_thread Task_handle;
+typedef void (*fork_t)(void* args);
+/**
+* C++ equivalent of the argolib_init API.
+*/
+void init(int argc, char **argv);
+/**
+* C++ equivalent of the argolib_finalize API.
+*/
+void finalize();
+// TODO FIGURE OUT LAMBDAS
+/**
+* C++ equivalent of the argolib_kernel API. User lambda passed instead of function pointer and args.
+*/
+// void kernel(T &&lambda);
+/**
+* C++ equivalent of the argolib_fork API. User lambda passed instead of function pointer and args.
+*/
+// Task_handle* fork(T &&lambda);
+/**
+* C++ equivalent of the argolib_join API.
+* All the task handles are passed as comma separated list. The three dots “...” represents variable
+* number of arguments.
+*/
+// void join(T... handles);
+} //end namespace argolib
+#endif
---
+++ src/argolib.c
@@ -0,0 +1,21 @@
+#include "argolib.h"
+
+// TODO_DOCUMENTATION
+void argolib_init(int argc, char **argv){
+}
+// TODO_DOCUMENTATION
+void argolib_finalize(){
+
+}
+// TODO_DOCUMENTATION
+void argolib_kernel(fork_t fptr, void* args){
+
+}
+// TODO_DOCUMENTATION
+Task_handle* argolib_fork(fork_t fptr, void* args){
+
+}
+// TODO_DOCUMENTATION
+void argolib_join(Task_handle** list, int size){
+
+}
