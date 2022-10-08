01b1e2eceecbb42e06ae7a6c95d79c6d6b34aee3
herschelle <herschelle19045@iiitd.ac.in>
2022-10-08 04:30:12 -0700

    Working Version of Argolib

---
+++ argolib.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include "argolib.h"
+
+// GLOBAL CONSTANTS
+#define DEFAULT_NUM_XSTREAMS 4
+
+// GLOBAL DECLERATIONS
+ABT_xstream *xstreams;
+ABT_pool *pools;
+ABT_sched *scheds;
+
+// TODO_DOCUMENTATION
+void argolib_init(int argc, char **argv) {
+    int i, j;
+    int num_xstreams = DEFAULT_NUM_XSTREAMS;
+
+    /* Allocate memory. */
+    xstreams = (ABT_xstream *) malloc(sizeof(ABT_xstream) * num_xstreams);
+    pools = (ABT_pool *) malloc(sizeof(ABT_pool) * num_xstreams);
+    scheds = (ABT_sched *) malloc(sizeof(ABT_sched) * num_xstreams);
+
+    /* Initialize Argobots. */
+    ABT_init(argc, argv);
+
+
+    /* Create pools. */
+    for (i = 0; i < num_xstreams; i++) {
+        ABT_pool_create_basic(ABT_POOL_FIFO, ABT_POOL_ACCESS_MPMC, ABT_TRUE, &pools[i]);
+    }
+
+    /* Create schedulers. */
+    for (i = 0; i < num_xstreams; i++) {
+        ABT_pool *tmp = (ABT_pool *) malloc(sizeof(ABT_pool) * num_xstreams);
+        for (j = 0; j < num_xstreams; j++) {
+            tmp[j] = pools[(i + j) % num_xstreams];
+        }
+        ABT_sched_create_basic(ABT_SCHED_RANDWS, num_xstreams, tmp,ABT_SCHED_CONFIG_NULL, &scheds[i]);
+        free(tmp);
+    }
+
+    /* Set up a primary execution stream. */
+    ABT_xstream_self(&xstreams[0]);
+    ABT_xstream_set_main_sched(xstreams[0], scheds[0]);
+
+    /* Create secondary execution streams. */
+    for (i = 1; i < num_xstreams; i++) {
+        ABT_xstream_create(scheds[i], &xstreams[i]);
+    }
+
+}
+
+// TODO_DOCUMENTATION
+void argolib_finalize() {
+    int i;
+    for (i = 1; i < DEFAULT_NUM_XSTREAMS; i++) {
+        ABT_xstream_join(xstreams[i]);
+        ABT_xstream_free(&xstreams[i]);
+    }
+
+    ABT_finalize();
+
+    free(xstreams);
+    free(pools);
+    free(scheds);
+}
+
+// TODO_DOCUMENTATION
+void argolib_kernel(fork_t fptr, void *args) {
+    fptr(&args);
+}
+
+// TODO_DOCUMENTATION
+Task_handle *argolib_fork(fork_t fptr, void *args) {
+
+    int rank;
+    ABT_xstream_self_rank(&rank);
+    ABT_pool target_pool = pools[rank];
+
+    Task_handle task;
+    ABT_thread_create(target_pool, fptr, args, ABT_THREAD_ATTR_NULL, &task);
+
+    Task_handle *task_ptr = (Task_handle *) malloc(sizeof(Task_handle));
+    *task_ptr = task;
+    return task_ptr;
+}
+
+
+// TODO_DOCUMENTATION
+void argolib_join(Task_handle *task) {
+    ABT_thread_free(task);
+}
---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "argolib.h"
+
+typedef struct {
+    int n;
+    int ret;
+} fib_arg_t;
+
+typedef ABT_thread Task_handle;
+
+void fib(void *arg) {
+    int n = ((fib_arg_t *) arg)->n;
+    int *p_ret = &((fib_arg_t *) arg)->ret;
+
+    if (n <= 1) {
+        *p_ret = 1;
+    } else {
+        fib_arg_t child1_arg = {n - 1, 0};
+        fib_arg_t child2_arg = {n - 2, 0};
+
+        /* Calculate fib(n - 1). */
+        Task_handle *task = argolib_fork(fib, &child1_arg);
+
+        /* Calculate fib(n - 2).  We do not create another ULT. */
+        fib(&child2_arg);
+
+        argolib_join(task);
+
+        *p_ret = child1_arg.ret + child2_arg.ret;
+    }
+}
+
+
+int main(int argc, char **argv) {
+    argolib_init(argc, argv);
+
+    int ret;
+    fib_arg_t arg = {10, 0};
+    fib(&arg);
+    ret = arg.ret;
+    printf("fib(10) = %d\n", ret);
+
+    argolib_finalize();
+
+    return 0;
+}
