f9ef7278ba16332171792fdd5b7113bd5a13bb4b
mayank18049 <mayank18049@iiitd.ac.in>
2022-10-09 16:38:31 +0530

    Update argolib.c

--- src/argolib.c
+++ src/argolib.c
@@ -1,67 +1,93 @@
 #include <stdlib.h>
-#include <argolib.h>
+#include "argolib.h"
+
 // GLOBAL CONSTANTS
-#define DEFAULT_NUM_XSTREAMS 1
+#define DEFAULT_NUM_XSTREAMS 4
+
 // GLOBAL DECLERATIONS
 ABT_xstream *xstreams;
 ABT_pool *pools;
 ABT_sched *scheds;
-Task_handle *top_task;
 
 // TODO_DOCUMENTATION
-void argolib_init(int argc, char **argv){
-    int i,j;
+void argolib_init(int argc, char **argv) {
+    int i, j;
     int num_xstreams = DEFAULT_NUM_XSTREAMS;
-    xstreams = (ABT_xstream *)malloc(sizeof(ABT_xstream) * num_xstreams);
-    pools = (ABT_pool *)malloc(sizeof(ABT_pool) * num_xstreams);
-    scheds = (ABT_sched *)malloc(sizeof(ABT_sched) * num_xstreams);
+
+    /* Allocate memory. */
+    xstreams = (ABT_xstream *) malloc(sizeof(ABT_xstream) * num_xstreams);
+    pools = (ABT_pool *) malloc(sizeof(ABT_pool) * num_xstreams);
+    scheds = (ABT_sched *) malloc(sizeof(ABT_sched) * num_xstreams);
+
+    /* Initialize Argobots. */
     ABT_init(argc, argv);
-    
-    for(i = 0;i<num_xstreams;i++){
-        ABT_pool_create_basic(ABT_POOL_RANDWS, ABT_POOL_ACCESS_MPMC, ABT_TRUE, &(pools[i]));
+
+
+    /* Create pools. */
+    for (i = 0; i < num_xstreams; i++) {
+        ABT_pool_create_basic(ABT_POOL_FIFO, ABT_POOL_ACCESS_MPMC, ABT_TRUE, &pools[i]);
     }
 
+    /* Create schedulers. */
     for (i = 0; i < num_xstreams; i++) {
-        ABT_pool *tmp = (ABT_pool *)malloc(sizeof(ABT_pool) * num_xstreams);
+        ABT_pool *tmp = (ABT_pool *) malloc(sizeof(ABT_pool) * num_xstreams);
         for (j = 0; j < num_xstreams; j++) {
             tmp[j] = pools[(i + j) % num_xstreams];
         }
-        ABT_sched_create_basic(ABT_SCHED_RANDWS, num_xstreams, tmp,
-                               ABT_SCHED_CONFIG_NULL, &(scheds[i]));
+        ABT_sched_create_basic(ABT_SCHED_RANDWS, num_xstreams, tmp,ABT_SCHED_CONFIG_NULL, &scheds[i]);
         free(tmp);
     }
-    ABT_xstream_self(&(xstreams[0]));
+
+    /* Set up a primary execution stream. */
+    ABT_xstream_self(&xstreams[0]);
     ABT_xstream_set_main_sched(xstreams[0], scheds[0]);
+
+    /* Create secondary execution streams. */
     for (i = 1; i < num_xstreams; i++) {
-        ABT_xstream_create(scheds[i], &(xstreams[i]));
+        ABT_xstream_create(scheds[i], &xstreams[i]);
     }
 
 }
+
 // TODO_DOCUMENTATION
-void argolib_finalize(){
-    ABT_thread_join(*top_task);
-    ABT_thread_free(top_task);
-    free(top_task);
+void argolib_finalize() {
+    int i;
+    for (i = 1; i < DEFAULT_NUM_XSTREAMS; i++) {
+        ABT_xstream_join(xstreams[i]);
+        ABT_xstream_free(&xstreams[i]);
+    }
+
     ABT_finalize();
+
+    free(xstreams);
+    free(pools);
+    free(scheds);
 }
+
 // TODO_DOCUMENTATION
-void argolib_kernel(fork_t fptr, void* args){
-    top_task = (Task_handle *)malloc(sizeof(Task_handle));
-    ABT_thread_create(pools[0], fptr, args,
-                          ABT_THREAD_ATTR_NULL, top_task);
+void argolib_kernel(fork_t fptr, void *args) {
+    fptr(args);
 }
+
 // TODO_DOCUMENTATION
-Task_handle* argolib_fork(fork_t fptr, void* args){
-    Task_handle *task;
-    task = (Task_handle *)malloc(sizeof(Task_handle));
-    ABT_thread_create(pools[0], fptr, args,
-                          ABT_THREAD_ATTR_NULL, top_task);
-    return task;
+Task_handle *argolib_fork(fork_t fptr, void *args) {
+
+    int rank;
+    ABT_xstream_self_rank(&rank);
+    ABT_pool target_pool = pools[rank];
+
+    Task_handle task;
+    ABT_thread_create(target_pool, fptr, args, ABT_THREAD_ATTR_NULL, &task);
+
+    Task_handle *task_ptr = (Task_handle *) malloc(sizeof(Task_handle));
+    *task_ptr = task;
+    return task_ptr;
 }
+
+
 // TODO_DOCUMENTATION
-void argolib_join(Task_handle** list, int size){
-    ABT_thread_join_many(size,*list);
-    for(int i =0;i<size;i++){
-        free(list[i]);
+void argolib_join(Task_handle **tasks, int size) {
+    for (int i = 0; i < size; ++i) {
+        ABT_thread_free(tasks[i]);
     }
 }
